dd8617c3045c8c763ea843d20487c43408ab254b
orangedy <orangedy@outlook.com>
2014-03-14 17:52:03 +0800

    add some note

--- src/test/FileTest.java
+++ src/test/FileTest.java
@@ -10,10 +10,14 @@ public class FileTest extends Thread {
 	public static void main(String[] args) throws IOException {
 		System.out.println(System.getProperty("user.dir"));
 		
+		//java io接口相对路径是相对于user.dir的，通常是项目根目录
+		//一般部署后单独存储的文件可以用这种方式或者绝对路径读取
 		File file = new File("1.txt");
 		System.out.println(file.getAbsolutePath());
 //		String dir = FileTest.class.getClassLoader().getResource("test/a.txt").toString();
-		String dir = FileTest.class.getResource("a.txt").getFile();
+		//classloader加载的相对路径是相对于classpath的，通常是项目bin目录或者target/classes目录即编译后文件目录
+		//一般同代码一起打包的文件通过这种方式读取
+		String dir = FileTest.class.getResource("/").getFile();
 		System.out.println(dir);
 		File filea = new File(dir);
 		System.out.println(filea.getAbsolutePath());
