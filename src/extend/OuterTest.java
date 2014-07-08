2e5bbbf3c8a7db88718c522ab6b00a0b5ca48e0e
hzdingyong <hzdingyong@HIH-D-2178.hz.ntes.domain>
2014-07-08 20:14:36 +0800

    test

--- src/extend/OuterTest.java
+++ src/extend/OuterTest.java
@@ -23,5 +23,6 @@ public class OuterTest {
     public static void main(String[] args){
         Outer.Inner in = new Outer.Inner();
         in.useOuter();
+        System.out.println("for test");
     }
 } 
