fc4c57663b1dd7ad5d05a2075cb097899beb3fb2
丁勇 <hzdingyong@corp.netease.com>
2014-11-04 21:25:15 +0800

    HttpClient和perf4j测试代码

---
+++ src/httpclient/HttpClient4Test.java
@@ -0,0 +1,18 @@
+/*
+ * @(#) HttpClient4Test.java 2014年11月4日
+ * 
+ * Copyright 2010 NetEase.com, Inc. All rights reserved.
+ */
+package httpclient;
+
+/**
+ *
+ * @author hzdingyong
+ * @version 2014年11月4日
+ */
+public class HttpClient4Test {
+
+    public void testPostRequest() {
+
+    }
+}
